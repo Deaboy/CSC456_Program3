f3c4abb14d927bd2302dd8698e85451364f5c03a
Deaboy <danieleandrus@gmail.com>
2015-05-04 02:28:58 -0600

    Added essential arguments exit and help

--- mmu.cpp
+++ mmu.cpp
@@ -4,10 +4,63 @@ int mmu(int argc, char*argv[])
 {
   vector<string> args;
   string input;
+  int temp;
   unsigned int i;
   unsigned int j;
   
-  // Print intro
+  unsigned int frame_size;
+  unsigned int frame_count;
+  unsigned int phys_frame_count;
+  unsigned char* memory;
+  
+  // Validate number of arguments
+  if (argc < 4)
+  {
+    cout << "Usage:\n\tmmu <frame size> <virtual frames> <physical frames>"
+         << endl;
+    return 0;
+  }
+  
+  // Parse and validate frame size
+  temp = (int) strtol(argv[1], NULL, 10);
+  if (temp < 1 || (int) log2((double) temp) != log2((double) temp))
+  {
+    cout << "Invalid frame size " << temp
+         << ": Expected positive power of 2" << endl;
+    return 0;
+  }
+  else
+  {
+    frame_size = (unsigned int) temp;
+  }
+  
+  // Parse and validate frame count
+  temp = (int) strtol(argv[2], NULL, 10);
+  if (temp < 1)
+  {
+    cout << "Invalid number of virtual frames " << temp
+         << ": Expected positive integer" << endl;
+    return 0;
+  }
+  else
+  {
+    frame_count = (unsigned int) temp;
+  }
+  
+  // Parse and validate physical frame count
+  temp = (int) strtol(argv[3], NULL, 10);
+  if (temp < 1)
+  {
+    cout << "Invalid number of physical frames " << temp
+         << ": Expected positive integer" << endl;
+    return 0;
+  }
+  else
+  {
+    phys_frame_count = (unsigned int) temp;
+  }
+  
+  // Print header
   cout << "Welcome to the Memory Management Unit (MMU) simulator.\n"
           "Please note that this is simulated command prompt with limited "
           "functionality.\nType 'help' for available commands." << endl;
