e30a79ec5d59d0ae0b233e6720d2615b636a4bbf
Joseph Mowry <joseph.mowry@mines.sdsmt.edu>
2015-05-04 05:14:36 -0600

    Stubs created for mmu functions, not yet implemented.

--- mmu.cpp
+++ mmu.cpp
@@ -2,6 +2,9 @@
 
 int mmu(int argc, char*argv[])
 {
+  cout << "This function is not fully implemented yet." << endl;
+  return 0;
+
   vector<string> args;
   string input;
   int temp;
@@ -11,7 +14,10 @@ int mmu(int argc, char*argv[])
   unsigned int frame_size;
   unsigned int frame_count;
   unsigned int phys_frame_count;
-  unsigned char* memory;
+  unsigned int phys_size;
+  unsigned char* phys_memory;
+  vector<unsigned char> swap_memory;
+  vector<unsigned int> page_table;
   
   // Validate number of arguments
   if (argc < 4)
@@ -60,12 +66,16 @@ int mmu(int argc, char*argv[])
     phys_frame_count = (unsigned int) temp;
   }
   
+  // Allocate memory
+  phys_size = phys_frame_count*frame_size;
+  phys_memory = new(nothrow)unsigned char[phys_size];
+
   // Print header
   cout << "Welcome to the Memory Management Unit (MMU) simulator.\n"
           "Please note that this is simulated command prompt with limited "
           "functionality.\nType 'help' for available commands." << endl;
   
-  
+
   do
   {
     args.clear();
@@ -101,13 +111,92 @@ int mmu(int argc, char*argv[])
       cout << "Available commands:\n"
               "\n"
               "exit\n"
-              "\tExits the MMU simulator\n";
+              "\tExits the MMU simulator\n"
+              "show <pt | vm | pm>\n"
+              "\tShows the page table, virtual memory, or physical memory\n";
       cout << endl;
+
     }
     else if (args[0] == "exit")
     {
       break;
     }
+    // 'Show' command and subcommands
+    else if (args[0] == "show")
+    {
+      if (args.size() < 2)
+      {
+        cout << "Incorrect usage." << endl;
+        continue;
+      }
+      // Page Table
+      else if (args[1] == "pt")
+      {
+        // Show page table Stats
+      }
+      // Virtual Memory
+      else if (args[1] == "vm")
+      {
+        // Show Virtual Memory Stats
+        cout << "Virtual Memory: " << frame_count*frame_size << "B" << endl;
+      }
+      // Physical Memory
+      else if (args[1] == "pm")
+      {
+        cout << "Physical Memory: " << phys_frame_count*frame_size <<"B" <<  endl;
+        //this many bytes
+      }
+      else
+      {
+        cout << "Unknown command '" << args[1] << "'" << endl;
+        continue;
+      }
+    }
+
+    else if (args[0] == "info")
+    {
+      cout << "Frame Size: " << frame_size << endl;
+      cout << "Frame Count: " << frame_count << endl;
+      cout << "Phys Frame Count: " << phys_frame_count << endl;
+
+      cout << "Physical Memory: " << phys_frame_count*frame_size <<"B" <<  endl;
+      cout << "Virtual Memory: " << frame_count*frame_size << "B" << endl;
+    
+    }
+
+    else if (args[0] == "dump")
+    {
+      //dump phys
+      
+      //dump swap space
+    }
+
+    else if (args[0] == "write")
+    {
+      if ( args.size() < 3 )
+      {
+        //nope
+      }
+    }
+
+    else if (args[0] == "read")
+    {
+      if ( args.size() < 2 )
+      {
+
+      }
+    }
+
+    else if (args[0] == "alloc")
+    {
+      
+    }
+
+    else if (args[0] == "dealloc")
+    {
+      
+    }
+
     else
     {
       cout << "Unknown command '" << args[0] << "'" << endl;
@@ -115,7 +204,12 @@ int mmu(int argc, char*argv[])
     
   }
   while(true);
-  
+  delete[]phys_memory;
   return 0;
 }
 
+int mmu_alloc (const vector<string> &args)
+{
+  
+  return 0;
+}
